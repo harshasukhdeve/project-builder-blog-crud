5d64bdccd894f635e5b7fc7bb8a346ab7c59b844
harshasukhdeve <harshasukhdeve0903@gmail.com>
2020-05-17 17:06:03 +0530

    FSJ0420A115

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -11,10 +11,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 import model.Blog;
 import model.User;
 import service.CRUDOperations;
-import service.ExcelFileStorage;
+//import service.ExcelFileStorage;
 import utility.CheckBlogPost;
 
 
@@ -61,11 +63,16 @@ public class BlogController extends HttpServlet {
 		
 		CRUDOperations crud=new CRUDOperations();
 		List<Blog> listblog = crud.createBlog(blog);
+		
+	
+		
+//		crud.viewBlog(blog);
+//		System.out.println("Sorted : "+crud.updateBlog(blog));
 
 		if(check) {
 			request.setAttribute("listBlog", listblog);
-//			request.setAttribute("blog", blog);
-//			request.setAttribute("user",user);
+			request.setAttribute("blog", blog);
+			request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
